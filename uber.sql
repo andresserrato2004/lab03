5075e67b57faf6efbd268ea1fb26689eb8840fd9
andresserrato2004 <andres.serrato-c@mail.escuelaing.edu.co>
2023-10-05 15:01:19 -0500

    caso ubersql

--- uber.sql
+++ uber.sql
@@ -19,6 +19,7 @@ estrellas NUMBER(1),
 estado CHAR(1)
 );
 
+
 CREATE TABLE persona(
 id NUMBER(9),
 tipo CHAR(2),
@@ -97,7 +98,15 @@ url VARCHAR(100)
 --CICLO 1: PoblarOK (1) 
 INSERT INTO Vehiculo (placa, a_o, tipo, estado, puertas, pasajeros, carga) VALUES ('ABC123', 2020, 'A', 'A', 4, 5, NULL);
 INSERT INTO Vehiculo (placa, a_o, tipo, estado, puertas, pasajeros, carga) VALUES ('XYZ789', 2019, 'B', 'A', 2, 2, NULL);
-INSERT INTO Vehiculo (placa, a_o, tipo, estado, puertas, pasajeros, carga) VALUES ('DEF456', 2022, 'C', 'I', NULL, NULL, 1000);
+INSERT INTO Vehiculo (placa, a_o, tipo, estado, puertas, pasajeros, carga) VALUES ('DEF456', 2022, 'C', 'I', NULL, NULL, 100);
 INSERT INTO Vehiculo (placa, a_o, tipo, estado, puertas, pasajeros, carga) VALUES ('GHI789', 2018, 'D', 'A', 4, 7, NULL);
 INSERT INTO Vehiculo (placa, a_o, tipo, estado, puertas, pasajeros, carga) VALUES ('JKL012', 2021, 'E', 'A', 2, NULL, 500);
 INSERT INTO Vehiculo (placa, a_o, tipo, estado, puertas, pasajeros, carga) VALUES ('MNO345', 2017, 'F', 'I', NULL, NULL, 800);
+
+--conductor(1)
+INSERT INTO conductor VALUES ('A12345', 1990-05-15, 5, 'A');
+INSERT INTO conductor VALUES ('B67890', 2022-04-22, 3, 'A');
+INSERT INTO conductor VALUES ('C54321', 1995-02-10, 4, 'I');
+INSERT INTO conductor VALUES ('D98765', 1988-11-30, 2, 'A');
+INSERT INTO conductor VALUES ('E24680', 1993-07-05, 4, 'A');
+INSERT INTO conductor VALUES ('F13579', 1998-04-20, 1, 'I');
